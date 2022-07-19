b23f1d08578e4fc04e4f11683e59b72a9ce8b893
0PatrickT0 <95145049+0PatrickT0@users.noreply.github.com>
2022-07-19 08:02:13 +0200

    update bdd 18072022

---
+++ mcd_sql/bdd_donkey_stay_18072022.sql
@@ -0,0 +1,8 @@
+INSERT INTO cottage (cottage_name, cottage_address, cottage_city, cottage_country, cottage_price_per_night, cottage_photo)
+VALUES ('MAISON MÉDIÉVALE', 'Catalogne', 'El Canós', 'Espagne', 120, 'https://a0.muscache.com/im/pictures/80af4642-c7dc-4bec-863d-fcbdf54cf0eb.jpg?im_w=1200'),
+('MAS DU XVÈME SIÈCLE', 'Occitanie', 'Arles-sur-Tech', 'France', 80, 'https://a0.muscache.com/im/pictures/miso/Hosting-32888733/original/ce8cd349-e143-47ba-8fff-9d7b26b3f57e.jpeg?im_w=1200'),
+('BELLE GRANGE RENOVEE', 'Occitanie', 'Loudervielle', 'France', 140, 'https://a0.muscache.com/im/pictures/57010138-b232-4536-b24d-9a376552bfeb.jpg?im_w=1200'),
+('CHÂTEAU D`ESCLAVELLES', 'Normandie', 'Esclavelles', 'France', 120, 'https://a0.muscache.com/im/pictures/miso/Hosting-606307396190257695/original/81944cd3-bba5-4522-9b52-b7edf005de8f.jpeg?im_w=1200'),
+('DOMAINE DE NOINTEL', 'Île-de-France', 'Nointel', 'France', 120, 'https://a0.muscache.com/im/pictures/miso/Hosting-626636234442140357/original/3f14ce83-0af9-43fa-8298-c6101c4d36f1.jpeg?im_w=1200'),
+('GRANGE RÉHABILITÉE', 'Hauts-de-France', 'Montépilloy', 'France', 120, 'https://a0.muscache.com/im/pictures/miso/Hosting-557702590908765257/original/04a08d1d-a946-4839-b3e7-f524d75fa25b.jpeg?im_w=1200'),
+('DOMAINE LE GROS CHÊNE', 'Normandie', 'Le Landin', 'France', 220, 'https://a0.muscache.com/im/pictures/5d37c4a0-d426-4f95-8264-f181a7b30e02.jpg?im_w=1200');
